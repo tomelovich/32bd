ad73fffae55ace587fcb836c15a0eb9fa79e4c20
tomelovich <tomelovichs@gmail.com>
2022-06-23 16:22:23 +0300

    Выполнено

---
+++ 32/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Part_32.Task_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridView4 = new System.Windows.Forms.DataGridView();
+            this.dataGridView5 = new System.Windows.Forms.DataGridView();
+            this.dataGridView6 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 15);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(587, 127);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(16, 149);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.Size = new System.Drawing.Size(957, 57);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(16, 282);
+            this.dataGridView3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.Size = new System.Drawing.Size(587, 145);
+            this.dataGridView3.TabIndex = 2;
+            // 
+            // dataGridView4
+            // 
+            this.dataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView4.Location = new System.Drawing.Point(16, 434);
+            this.dataGridView4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView4.Name = "dataGridView4";
+            this.dataGridView4.RowHeadersWidth = 51;
+            this.dataGridView4.Size = new System.Drawing.Size(587, 145);
+            this.dataGridView4.TabIndex = 3;
+            // 
+            // dataGridView5
+            // 
+            this.dataGridView5.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView5.Location = new System.Drawing.Point(611, 434);
+            this.dataGridView5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView5.Name = "dataGridView5";
+            this.dataGridView5.RowHeadersWidth = 51;
+            this.dataGridView5.Size = new System.Drawing.Size(587, 145);
+            this.dataGridView5.TabIndex = 5;
+            // 
+            // dataGridView6
+            // 
+            this.dataGridView6.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView6.Location = new System.Drawing.Point(611, 282);
+            this.dataGridView6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView6.Name = "dataGridView6";
+            this.dataGridView6.RowHeadersWidth = 51;
+            this.dataGridView6.Size = new System.Drawing.Size(587, 145);
+            this.dataGridView6.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1208, 587);
+            this.Controls.Add(this.dataGridView5);
+            this.Controls.Add(this.dataGridView6);
+            this.Controls.Add(this.dataGridView4);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView6)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView4;
+        private System.Windows.Forms.DataGridView dataGridView5;
+        private System.Windows.Forms.DataGridView dataGridView6;
+    }
+}
+
---
+++ 32/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+
+using System.Windows.Forms;
+
+namespace Part_32.Task_1
+{
+    public partial class Form1 : Form
+    {
+        private const string _conString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\практика\32\Part_32.Task_1\Lab30.accdb";
+        private readonly DataSet _dataSet;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _dataSet = new DataSet();
+            Load += FormMain_Load;
+
+            dataGridView1.SelectionChanged += DataGridViewMaster_SelectionChanged;
+            
+            using (var adpa = new OleDbDataAdapter("SELECT * FROM ИнформацияОТуристах", _conString))
+            using (var adpb = new OleDbDataAdapter("SELECT * FROM Туристы", _conString))
+            using (var adp1 = new OleDbDataAdapter("SELECT * FROM Оплата", _conString))
+            using (var adp2 = new OleDbDataAdapter("SELECT * FROM Путевки", _conString))
+            using (var adp3 = new OleDbDataAdapter("SELECT * FROM Сезон", _conString))
+            using (var adp4 = new OleDbDataAdapter("SELECT * FROM Туры", _conString))
+            {
+                adpa.Fill(_dataSet, "ИнформацияОТуристах");
+                adpb.Fill(_dataSet, "Туристы");
+                adp1.Fill(_dataSet, "Оплата");
+                adp2.Fill(_dataSet, "Путевки");
+                adp3.Fill(_dataSet, "Сезон");
+                adp4.Fill(_dataSet, "Туры");
+            }
+
+            dataGridView3.DataSource = _dataSet.Tables["Оплата"];
+            dataGridView4.DataSource = _dataSet.Tables["Путевки"];
+            dataGridView5.DataSource = _dataSet.Tables["Сезон"];
+            dataGridView6.DataSource = _dataSet.Tables["Туры"];
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            _dataSet.Tables[0].Constraints.Add("КодТуриста", _dataSet.Tables[0].Columns[0], true);
+            _dataSet.Tables[1].Constraints.Add("КодТуриста", _dataSet.Tables[1].Columns[0], true);
+
+            _dataSet.Relations.Add("ИнформацияТуриста", _dataSet.Tables[1].Columns[0], _dataSet.Tables[0].Columns[0]);
+
+            dataGridView1.DataSource = _dataSet.Tables["Туристы"];
+            LoadChildData(0);
+
+            dataGridView1.SelectionChanged += DataGridViewMaster_SelectionChanged;
+        }
+
+        private void LoadChildData(int rowIndex)
+        {
+            var parentRow = _dataSet.Tables["Туристы"].Rows[rowIndex];
+            var childRows = parentRow.GetChildRows("ИнформацияТуриста");
+            var childTable = _dataSet.Tables["ИнформацияОТуристах"].Clone();
+
+            foreach (var row in childRows)
+            {
+                childTable.ImportRow(row);
+            }
+
+            dataGridView2.DataSource = childTable;
+        }
+
+        private void DataGridViewMaster_SelectionChanged(object sender, EventArgs e)
+        {
+            LoadChildData(dataGridView1.CurrentRow.Index);
+        }
+
+    }
+}
